7ae3c4870a7c264140d43701e001c005fab39c0e
Estudiantes <Estudiantes@labing50210.udistrital.edu.co>
2017-05-12 17:18:39 -0500

    Clase Pixel

---
+++ uAnalítica/src/uanalítica/Pixel.java
@@ -0,0 +1,32 @@
+package uanalítica;
+
+// NOTE: please use a PRESERVE:BEGIN/PRESERVE:END comment block
+//       to preserve your hand-coding across code generations.
+
+public class Pixel {
+
+	public void setR() {
+		// PRESERVE:BEGIN
+		// Insert your preservable code here...
+		// PRESERVE:END
+	}
+
+	public void setG() {
+		// PRESERVE:BEGIN
+		// Insert your preservable code here...
+		// PRESERVE:END
+	}
+
+	public void setB() {
+		// PRESERVE:BEGIN
+		// Insert your preservable code here...
+		// PRESERVE:END
+	}
+
+	private  R;
+
+	private  G;
+
+	private  B;
+
+}
